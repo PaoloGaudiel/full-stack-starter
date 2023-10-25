4b23c027d8003a58b7887128dc55842b7a686ea1
Paolo Gaudiel <135890965+PaoloGaudiel@users.noreply.github.com>
2023-10-24 20:52:22 -0700

    Added an Idols API route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -5,10 +5,13 @@ import invitesRoutes from './invites.js';
 import passwordsRoutes from './passwords.js';
 import usersRoutes from './users.js';
 
+import idolsRoutes from './idols.js';
+
 const router = express.Router();
 
 router.use('/assets', assetsRoutes);
 router.use('/auth', authRoutes);
+router.use('/idols', idolsRoutes);
 router.use('/invites', invitesRoutes);
 router.use('/passwords', passwordsRoutes);
 router.use('/users', usersRoutes);
--- server/test/models/idol.js
+++ server/test/models/idol.js
@@ -8,11 +8,12 @@ import models from '../../models/index.js';
 
 describe('models.Idol', () => {
   beforeEach(async () => {
-    await helper.loadFixtures([]);
+    await helper.loadFixtures(['idols']);
   });
 
   it('creates a new Idol record', async () => {
-    assert.deepStrictEqual(await models.Idol.count(), 0);
+    assert.deepStrictEqual(await models.Idol.count(), 2);
+
     const record = await models.Idol.create({
         stageName: 'Stage Name',
         groupName: 'Group Name',
@@ -25,7 +26,8 @@ describe('models.Idol', () => {
         portrait: 'Portrait URL',
         groupPicture: 'Group Picture URL'
     });
-    assert.deepStrictEqual(await models.Idol.count(), 1);
+
+    assert.deepStrictEqual(await models.Idol.count(), 3);
     assert.notDeepStrictEqual(record.id, null);
     assert.deepStrictEqual(record.stageName, 'Stage Name');
     assert.deepStrictEqual(record.groupName, 'Group Name');
@@ -37,5 +39,27 @@ describe('models.Idol', () => {
     assert.deepStrictEqual(record.repEmoji,'EMOJI');
     assert.deepStrictEqual(record.portrait, 'Portrait URL');
     assert.deepStrictEqual(record.groupPicture, 'Group Picture URL');
+
+  });
+
+  it('finds an Idol record by its id', async() => {
+    const record = await models.Idol.findByPk(10000);
+    assert.notDeepStrictEqual(record, null);
+    assert.deepStrictEqual(record.stageName, 'Stage Name 1');
+  });
+
+  it('finds multiple Idol records', async() => {
+    const records = await models.Idol.findAll({
+      order: [['stageName', 'ASC']]
+    });
+    assert.deepStrictEqual(records.length, 2);
+    assert.deepStrictEqual(records[0].stageName, 'Stage Name 1');
+  });
+
+  it('deletes an Idol record', async() => {
+    assert.deepStrictEqual(await models.Idol.count(), 2);
+    const record = await models.Idol.findByPk(10000);
+    await record.destroy();
+    assert.deepStrictEqual(await models.Idol.count(), 1);
   });
 });
