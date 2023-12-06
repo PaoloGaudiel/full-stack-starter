9e0ab4b7b202997d72eae5384cba48bf3133ab2f
Paolo Gaudiel <135890965+PaoloGaudiel@users.noreply.github.com>
2023-12-05 19:05:35 -0800

    Hide Delete button from form when creating a new Idol

--- client/src/IdolForm.jsx
+++ client/src/IdolForm.jsx
@@ -113,7 +113,7 @@ function IdolForm() {
                 </div>
                 <div className="d-flex justify-content-between">
                     <button type="submit" className="btn btn-primary">Submit</button>
-                    <button onClick={showConfirmDeleteModal} type="button" className="btn btn-danger">Delete</button>
+                    {id && <button onClick={showConfirmDeleteModal} type="button" className="btn btn-danger">Delete</button>}
                 </div>
                 <Modal centered show={isConfirmDeleteShowing} onHide={handleClose}>
                     <Modal.Header closeButton>
